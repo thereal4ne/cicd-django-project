207937c3c5dadfb12120c0731f4661df8d67835f
Albin Jacob <albinjacob157@gmail.com>
2026-02-04 07:29:26 +0530

    Fix root URL mapping

--- cicdproject/urls.py
+++ cicdproject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('webapp.urls')),  # <-- ADD THIS LINE
+    path('', include('webapp.urls')),   # root URL
     path('admin/', admin.site.urls),
 ]
