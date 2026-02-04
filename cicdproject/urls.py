14d72073ee84d9835d7d4dd078891f9eb9c891de
Albin Jacob <albinjacob157@gmail.com>
2026-02-04 18:44:52 +0530

    Fix broken urls.py syntax

--- cicdproject/urls.py
+++ cicdproject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('webapp.urls')),   # root URL
+    path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
 ]
