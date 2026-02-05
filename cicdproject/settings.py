28087b3ff0b1d414a9fbf68993e283c4a2659d9b
Albin Jacob <albinjacob157@gmail.com>
2026-02-05 07:24:06 +0530

    Move Django settings to environment variables

--- cicdproject/settings.py
+++ cicdproject/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njfa!l2yq8z6z1*$2#rg(p6-^16i2(6u0=6ep(kyg_as56dkxi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
