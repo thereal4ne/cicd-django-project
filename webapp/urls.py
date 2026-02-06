def4cf236d18710e5a66ee22ffffa361f36b000c
Albin Jacob <albinjacob157@gmail.com>
2026-02-06 09:38:58 +0530

    Added complete and delete task features with Bootstrap UI

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, complete_task, delete_task
 
 urlpatterns = [
-    # Change 'tasks/' to '' to make it the homepage
     path('', task_list, name='task_list'),
+    path('complete/<int:task_id>/', complete_task, name='complete_task'),
+    path('delete/<int:task_id>/', delete_task, name='delete_task'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def task_list(request):
@@ -10,3 +10,16 @@ def task_list(request):
 
     tasks = Task.objects.all()
     return render(request, "home.html", {"tasks": tasks})
+
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect("task_list")
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("task_list")
