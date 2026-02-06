de6489adfdda79eb1137585d4c71bdbe4436bfe2
Albin Jacob <albinjacob157@gmail.com>
2026-02-06 09:14:28 +0530

    Added task form and database integration

--- cicdproject/settings.py
+++ cicdproject/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'test-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
 
 
 # Application definition
